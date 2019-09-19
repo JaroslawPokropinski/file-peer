91a7b48f85ac8823c04fd8d617dabc5759964368
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-09-19 16:39:53 +0200

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
-var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({
-	port: process.env.PORT || 9000,
-	key: 'peerjs',
-	path: '/myapp',
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const ExpressPeerServer = require('peer').ExpressPeerServer;
+
+const app = express();
+
+// Express configuration
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
+//
+
+const options = {
+  port: process.env.PORT || 9000,
+  expire_timeout: 5000,
+  alive_timeout: 60000,
+  key: 'peerjs',
+  path: '/myapp',
+  concurrent_limit: 5000,
+  allow_discovery: false,
+  proxied: false,
+  cleanup_out_msgs: 1000,
+  ssl: {
+    key: '',
+    cert: '',
+  },
+};
+
+let server;
+if (options.ssl && options.ssl.key && options.ssl.cert) {
+  server = https.createServer(options.ssl, app);
+  delete options.ssl;
+} else {
+  server = http.createServer(app);
+}
+
+const peerjs = ExpressPeerServer(server, options);
+app.use(peerjs);
+
+server.listen(options.port);
