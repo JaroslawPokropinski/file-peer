a583b1a98ece5a00ead43853d2ac9bc585919771
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-09-19 22:03:10 +0200

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const http = require('http');
 const https = require('https');
+const cors = require('cors');
 const ExpressPeerServer = require('peer').ExpressPeerServer;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 // Express configuration
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+let users = new Map();
+app.put('/user', (req, res, next) => {
+  if (!req.body.id || !req.body.name) {
+    next('Bad parameters!');
+    return;
+  }
+  users.set(req.body.name, req.body.id);
+  console.log(`${req.body.name} connected`);
+  res.send();
+});
+
+app.get('/users', (req, res) => {
+  let re = [];
+  for (let user of users.keys()) {
+    re.push(user);
+  }
+  res.send(re);
 });
 //
 
